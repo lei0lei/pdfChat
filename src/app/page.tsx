3238e1d9b239b228382c4bcddcae15562b2aa1df
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-13 01:38:18 +0800

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import 'react-chatbot-kit/build/main.css'
 
 // import { Navigation } from './Navigation.js'
 import React from 'react';
-import '@/app/App.css'
+import '@/app/app.css'
 import { PdfProvider } from './provider.js';
 // import PdfViewer from './PdfViewerx.js'
 import dynamic from 'next/dynamic';
