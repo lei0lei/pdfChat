906b2304a4df5fd734074052cebe620029a4ae5b
lei0lei <lei.lei.fan.meng@gmail.com>
2024-01-30 12:36:58 +0800

    .

--- src/components/PdfViewer/pdfViewer.tsx
+++ src/components/PdfViewer/pdfViewer.tsx
@@ -97,6 +97,7 @@ const PdfViewerx = (
                                         !currentShowFileObj._file&&(<div className="flex flex-col items-center justify-center space-y-4 bg-white shadow-md rounded-md p-6">
                                         <DocumentIcon className="w-24 h-24 text-blue-500" />
                                         <p className="text-lg font-medium text-gray-700">未选择文件</p>
+                                        <p className="text-lg font-medium text-gray-700">(单文件不超过5MB)</p>
                                       </div>)}
                                     </div>
                                 </div>
